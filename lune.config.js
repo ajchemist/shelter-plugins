490fa4cc748791b518f7a095434f3a52ad3ab10f
ajchemist <1694505+aJchemist@users.noreply.github.com>
2026-05-31 01:53:31 +0900

    Remove base_url so lune SSG uses runtime page URL for install link

--- lune.config.js
+++ lune.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from "@uwu/lune";
 export default defineConfig({
 	ssg: {
 		repo_name: "shelter-plugins",
-		base_url: "https://ajchemist.github.io/shelter-plugins",
 	},
 });
